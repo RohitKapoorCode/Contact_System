16f4c0f03b54bb666cfa5caaf60dbde8a6793514
Rohit Kapoor <rk7612570@gmail.com>
2026-09-20 18:24:09 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,27 +1,25 @@
+
+
+
 <?php
-
-ob_start(); 
-
 if(isset($_POST['submit'])){
     $fullName = $_POST['first_name'];
     $phone = $_POST['phone'];
     $email = $_POST['email'];
-    $save = $_POST['ContactSave'];
+    $save =$_POST['ContactSave'];
 
 
-    $conn = mysqli_connect("mysql-4acf231-rk7612570-43fe.g.aivencloud.com", "avnadmin", getenv('DB_PASSWORD'), "defaultdb", 27958) or die("Connection Failed");
+ $conn = mysqli_connect("mysql-4acf231-rk7612570-43fe.g.aivencloud.com", "avnadmin", getenv('DB_PASSWORD'), "defaultdb", 27958) or die("Connection Failed");
 
- 
-    $sql = "INSERT INTO contact(FullName, Phone, Email, Save) VALUES ('{$fullName}', '{$phone}', '{$email}', '{$save}')";
-    $result = mysqli_query($conn, $sql);
-
-
+    $Sql = "INSERT INTO contact(FullName,Phone,Email,Save) VALUES ('{$fullName}', '{$phone}', '{$email}','{$save}')";
+    $result = mysqli_query($conn,$Sql);
     mysqli_close($conn);
 
-  
     echo "<script>window.location.href='http://localhost/Rohit%20Project/contact%20Project/home.php';</script>";
-    exit();
+    exit;
 }
 
 
 ?>
+    
+
