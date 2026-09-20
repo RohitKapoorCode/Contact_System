a3ff426a0cd82fa6c76eb96dc22c35c854f99bd3
Rohit Kapoor <rk7612570@gmail.com>
2026-09-20 18:15:55 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,26 +1,24 @@
 
-<?php
-
-ob_start(); 
+<?php ob_start(); 
 
 
-  if(isset($_POST['submit'])){
-  $fullName = $_POST['first_name'];
-  $phone = $_POST['phone'];
-  $email = $_POST['email'];
-  $save =$_POST['ContactSave'];
-
-
- 
-  $conn = mysqli_connect("mysql-4acf231-rk7612570-43fe.g.aivencloud.com", "avnadmin", getenv('DB_PASSWORD'), "defaultdb", 27958) or die("Connection Failed");
-
-
-    $Sql = "INSERT INTO contact(FullName,Phone,Email,Save) VALUES ('{$fullName}', '{$phone}', '{$email}','{$save}')";
-    $result = mysqli_query($conn,$Sql);
-    mysqli_close($conn);
-
-     header("Location: http://localhost/Rohit%20Project/contact%20Project/home.php");
-     exit;
+      if(isset($_POST['submit'])){
+      $fullName = $_POST['first_name'];
+      $phone = $_POST['phone'];
+      $email = $_POST['email'];
+      $save =$_POST['ContactSave'];
+    
+    
+     
+      $conn = mysqli_connect("mysql-4acf231-rk7612570-43fe.g.aivencloud.com", "avnadmin", getenv('DB_PASSWORD'), "defaultdb", 27958) or die("Connection Failed");
+    
+    
+        $Sql = "INSERT INTO contact(FullName,Phone,Email,Save) VALUES ('{$fullName}', '{$phone}', '{$email}','{$save}')";
+        $result = mysqli_query($conn,$Sql);
+        mysqli_close($conn);
+    
+        header("Location: http://localhost/Rohit%20Project/contact%20Project/home.php");
+        exit;
 }
     
 
