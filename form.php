7b936b88a5b00484f8538185bb986cad72e00fbf
Rohit Kapoor <rk7612570@gmail.com>
2026-09-20 16:01:23 +0530

    Update form.php

--- form.php
+++ form.php
@@ -120,7 +120,9 @@
                   <select name="ContactSave" id="ContactSave" class="form-control" required>
                     <option value="" selected disabled>Select Device/Location</option>
                     <?php
-                        $conn=mysqli_connect("localhost","root","","contact_project") or die("Connection Failed");
+                        
+                        $conn = mysqli_connect("mysql-4acf231-rk7612570-43fe.g.aivencloud.com", "avnadmin", getenv('DB_PASSWORD'), "defaultdb", 27958) or die("Connection Failed");
+
 
                         $Sql ="SELECT * FROM save";
                         $result = mysqli_query($conn,$Sql) or die("Query Unsuccessful");
